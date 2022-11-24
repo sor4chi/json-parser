fe033090c7ab4517ccd7aaceabebf84fab4765e5
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-11-25 01:44:26 +0900

    feat: tokenizer (v1)

---
+++ src/token.rs
@@ -0,0 +1,277 @@
+use phf::phf_map;
+use std::iter::Peekable;
+use std::vec::IntoIter;
+
+pub type PeekableIter<T> = Peekable<IntoIter<T>>;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum Token {
+    LBrace,
+    RBrace,
+    LBracket,
+    RBracket,
+    Colon,
+    Comma,
+    String,
+    Number,
+    True,
+    False,
+    Null,
+    End
+}
+
+static CHAR_TOKENS: phf::Map<char, Token> = phf_map! {
+    '{' => Token::LBrace,
+    '}' => Token::RBrace,
+    '[' => Token::LBracket,
+    ']' => Token::RBracket,
+    ':' => Token::Colon,
+    ',' => Token::Comma,
+};
+
+static KEYWORD_TOKENS: phf::Map<&'static str, Token> = phf_map! {
+    "true" => Token::True,
+    "false" => Token::False,
+    "null" => Token::Null,
+};
+
+pub struct Tokenizer {
+    char_stream: PeekableIter<char>,
+}
+
+impl Tokenizer {
+    pub fn new(input: &str) -> Self {
+        let vec: Vec<char> = input.chars().collect();
+        let char_stream = vec.into_iter().peekable();
+        Tokenizer { char_stream }
+    }
+
+    pub fn peek_char(&mut self) -> Option<&char> {
+        self.char_stream.peek()
+    }
+
+    pub fn next_char(&mut self) -> Option<char> {
+        self.char_stream.next()
+    }
+
+    pub fn consume_char(&mut self) -> Token {
+        match self.next_char() {
+            Some(c) => match CHAR_TOKENS.get(&c) {
+                Some(token) => token.clone(),
+                None => panic!("Unexpected character: {}", c),
+            },
+            None => panic!("Unexpected char of input"),
+        }
+    }
+
+    pub fn consume_string(&mut self) -> Token {
+        if self.peek_char() == Some(&'"') {
+            self.next_char(); // the first "
+        }
+        loop {
+            match self.next_char() {
+                Some('"') => break,
+                Some(_) => continue,
+                None => panic!("Unexpected end of input"),
+            }
+        }
+        Token::String
+    }
+
+    pub fn consume_number(&mut self) -> Token {
+        loop {
+            match self.peek_char() {
+                Some(c) if c.is_numeric() => {
+                    self.next_char();
+                }
+                _ => break,
+            }
+        }
+        Token::Number
+    }
+
+    pub fn consume_keyword(&mut self) -> Token {
+        let mut keyword = String::new();
+        loop {
+            let c = self.peek_char();
+            match c {
+                Some(c) if c.is_alphanumeric() => {
+                    keyword.push(*c);
+                    self.next_char();
+                }
+                _ => break,
+            }
+        }
+        match KEYWORD_TOKENS.get(&keyword[..]) {
+            Some(token) => token.clone(),
+            None => panic!("Unexpected keyword: {}", keyword),
+        }
+    }
+
+    pub fn consume_whitespace(&mut self) {
+        loop {
+            match self.peek_char() {
+                Some(c) if c.is_whitespace() => {
+                    self.next_char();
+                }
+                _ => break,
+            }
+        }
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        self.consume_whitespace();
+        let c = self.peek_char();
+        match c {
+            Some(c) => match c {
+                '{' | '}' | '[' | ']' | ':' | ',' => self.consume_char(),
+                '"' => self.consume_string(),
+                '0'..='9' => self.consume_number(),
+                'a'..='z' | 'A'..='Z' => self.consume_keyword(),
+                _ => panic!("Unexpected character: {}", c),
+            },
+            None => Token::End,
+        }
+    }
+
+    pub fn tokenize(&mut self) -> Vec<Token> {
+        let mut tokens = Vec::new();
+        loop {
+            let token = self.next_token();
+            tokens.push(token.clone());
+            if token == Token::End {
+                break;
+            }
+        }
+        tokens
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_peek_char() {
+        let input = r#"{"foo":123}"#;
+        let mut tokenizer = Tokenizer::new(input);
+        assert_eq!(tokenizer.peek_char(), Some(&'{'));
+        assert_eq!(tokenizer.peek_char(), Some(&'{'));
+    }
+
+    #[test]
+    fn test_next_char() {
+        let input = r#"{"foo":123}"#;
+        let mut tokenizer = Tokenizer::new(input);
+        assert_eq!(tokenizer.next_char(), Some('{'));
+        assert_eq!(tokenizer.next_char(), Some('"'));
+        assert_eq!(tokenizer.next_char(), Some('f'));
+        assert_eq!(tokenizer.next_char(), Some('o'));
+        assert_eq!(tokenizer.next_char(), Some('o'));
+        assert_eq!(tokenizer.next_char(), Some('"'));
+        assert_eq!(tokenizer.next_char(), Some(':'));
+        assert_eq!(tokenizer.next_char(), Some('1'));
+        assert_eq!(tokenizer.next_char(), Some('2'));
+        assert_eq!(tokenizer.next_char(), Some('3'));
+        assert_eq!(tokenizer.next_char(), Some('}'));
+        assert_eq!(tokenizer.next_char(), None);
+    }
+
+    #[test]
+    fn test_consume_char() {
+        let input = r#"{}[]:,"#;
+        let mut tokenizer = Tokenizer::new(input);
+        assert_eq!(tokenizer.consume_char(), Token::LBrace); // {
+        assert_eq!(tokenizer.consume_char(), Token::RBrace); // }
+        assert_eq!(tokenizer.consume_char(), Token::LBracket); // [
+        assert_eq!(tokenizer.consume_char(), Token::RBracket); // ]
+        assert_eq!(tokenizer.consume_char(), Token::Colon); // :
+        assert_eq!(tokenizer.consume_char(), Token::Comma); // ,
+    }
+
+    #[test]
+    fn test_consume_string() {
+        let input = r#"{"foo":"bar"}"#;
+        let mut tokenizer = Tokenizer::new(input);
+        assert_eq!(tokenizer.consume_char(), Token::LBrace); // {
+        assert_eq!(tokenizer.consume_string(), Token::String); // "foo"
+        assert_eq!(tokenizer.consume_char(), Token::Colon); // :
+        assert_eq!(tokenizer.consume_string(), Token::String); // "bar"
+        assert_eq!(tokenizer.consume_char(), Token::RBrace); // }
+    }
+
+    #[test]
+    fn test_consume_number() {
+        let input = r#"{"foo":123}"#;
+        let mut tokenizer = Tokenizer::new(input);
+        assert_eq!(tokenizer.consume_char(), Token::LBrace); // {
+        assert_eq!(tokenizer.consume_string(), Token::String); // "foo"
+        assert_eq!(tokenizer.consume_char(), Token::Colon); // :
+        assert_eq!(tokenizer.consume_number(), Token::Number); // 123
+        assert_eq!(tokenizer.consume_char(), Token::RBrace); // }
+    }
+
+    #[test]
+    fn test_consume_keyword() {
+        let input = r#"{"foo":true,"bar":false,"baz":null}"#;
+        let mut tokenizer = Tokenizer::new(input);
+        assert_eq!(tokenizer.consume_char(), Token::LBrace); // {
+        assert_eq!(tokenizer.consume_string(), Token::String); // "foo"
+        assert_eq!(tokenizer.consume_char(), Token::Colon); // :
+        assert_eq!(tokenizer.consume_keyword(), Token::True); // true
+        assert_eq!(tokenizer.consume_char(), Token::Comma); // ,
+        assert_eq!(tokenizer.consume_string(), Token::String); // "bar"
+        assert_eq!(tokenizer.consume_char(), Token::Colon); // :
+        assert_eq!(tokenizer.consume_keyword(), Token::False); // false
+        assert_eq!(tokenizer.consume_char(), Token::Comma); // ,
+        assert_eq!(tokenizer.consume_string(), Token::String); // "baz"
+        assert_eq!(tokenizer.consume_char(), Token::Colon); // :
+        assert_eq!(tokenizer.consume_keyword(), Token::Null); // null
+        assert_eq!(tokenizer.consume_char(), Token::RBrace); // }
+    }
+
+    #[test]
+    fn test_consume_whitespace() {
+        let input = r#"{    "foo": 123
+        }"#;
+        let mut tokenizer = Tokenizer::new(input);
+        assert_eq!(tokenizer.consume_char(), Token::LBrace); // {
+        tokenizer.consume_whitespace(); // tab whitespace
+        assert_eq!(tokenizer.consume_string(), Token::String); // "foo"
+        assert_eq!(tokenizer.consume_char(), Token::Colon); // :
+        tokenizer.consume_whitespace(); // space whitespace
+        assert_eq!(tokenizer.consume_number(), Token::Number); // 123
+        tokenizer.consume_whitespace(); // new line whitespace
+        assert_eq!(tokenizer.consume_char(), Token::RBrace); // }
+    }
+
+    #[test]
+    fn test_next_token() {
+        let input = r#"{"foo":123}"#;
+        let mut tokenizer = Tokenizer::new(input);
+        assert_eq!(tokenizer.next_token(), Token::LBrace); // {
+        assert_eq!(tokenizer.next_token(), Token::String); // "foo"
+        assert_eq!(tokenizer.next_token(), Token::Colon); // :
+        assert_eq!(tokenizer.next_token(), Token::Number); // 123
+        assert_eq!(tokenizer.next_token(), Token::RBrace); // }
+        assert_eq!(tokenizer.next_token(), Token::End); // end
+    }
+
+    #[test]
+    fn test_tokenize() {
+        let input = r#"{"foo":123}"#;
+        let mut tokenizer = Tokenizer::new(input);
+        let tests: Vec<Token> = vec![
+            Token::LBrace, // {
+            Token::String, // "foo"
+            Token::Colon, // :
+            Token::Number, // 123
+            Token::RBrace, // }
+            Token::End, // end
+        ];
+
+        for test in tests {
+            assert_eq!(tokenizer.next_token(), test);
+        }
+    }
+}
